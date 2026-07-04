d4ecd5ac1da5176cdcff096987129664e591f839
Sara Manzoor <saramanzoor76@gmail.com>
2026-07-04 10:08:40 +0500

    Day 2: Added list comprehensions practice

---
+++ Module 1(Python Fundamentals)/Day_2/list_Comprehension.py
@@ -0,0 +1,16 @@
+# Squares
+
+squares = [x*x for x in range(10)]
+print(squares)
+
+# Even numbers
+
+even = [x for x in range(20) if x % 2 == 0]
+print(even)
+
+# Uppercase
+
+names = ["ali", "sara", "ahmed"]
+
+upper = [name.upper() for name in names]
+print(upper)
