2456184f53a2f7f7c4d890ee06451e1c98a4372c
Sara Manzoor <saramanzoor76@gmail.com>
2026-07-04 10:08:28 +0500

    Day 2: Added functions (def, return, docstrings)

---
+++ Module 1(Python Fundamentals)/Day_2/functions.py
@@ -0,0 +1,27 @@
+# Simple function
+
+def greet():
+    print("Hello")
+
+greet()
+
+# Function with parameter
+
+def square(x):
+    return x * x
+
+print(square(6))
+
+# Default argument
+
+def welcome(name="Guest"):
+    print(name)
+
+welcome()
+
+# Multiple parameters
+
+def add(a, b):
+    return a + b
+
+print(add(5, 10))
