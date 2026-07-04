6007a1caeff902c1c645d5aadeddf37e4c6c9512
Sara Manzoor <saramanzoor76@gmail.com>
2026-07-04 15:02:58 +0500

    Add better comments

--- Module 1(Python Fundamentals)/Day_2/lambda_functions.py
+++ Module 1(Python Fundamentals)/Day_2/lambda_functions.py
@@ -1,13 +1,15 @@
 square = lambda x: x*x
-
+# lambda parameters: expression
 print(square(8))
 
 numbers = [1,2,3,4,5]
 
 double = list(map(lambda x:x*2, numbers))
-
+# map() applies a function to every element of an iterable (like a list).
+# map(function, iterable)
 print(double)
 
 even = list(filter(lambda x:x%2==0, numbers))
-
+# filter() keeps only the elements for which the function returns True.
+# filter(function, iterable)
 print(even)
