2fda047858a6fb4e31d0603635305176d48c3286
Sara Manzoor <saramanzoor76@gmail.com>
2026-07-04 10:08:02 +0500

    Day 2: Added built-in functions examples (abs, round, help, etc.)

---
+++ Module 1(Python Fundamentals)/Day_2/built_in_functions.py
@@ -0,0 +1,12 @@
+numbers = [5, 2, 9, 1]
+
+print(len(numbers))
+print(max(numbers))
+print(min(numbers))
+print(sum(numbers))
+print(sorted(numbers))
+
+print(type(numbers))
+
+for index, value in enumerate(numbers):
+    print(index, value)
